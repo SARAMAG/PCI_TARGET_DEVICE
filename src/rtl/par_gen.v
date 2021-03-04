6766a5e8ad77205191485e0644942435343d9909
Sara Magdi <58006432+SARAMAG@users.noreply.github.com>
2021-03-04 17:18:29 +0200

    Update par_gen.v

--- src/rtl/par_gen.v
+++ src/rtl/par_gen.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:11:30 02/20/2021 
-// Design Name: 
-// Module Name:    par_gen 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+// PARITY GENERATOR 
 //////////////////////////////////////////////////////////////////////////////////
 module par_gen(
     input [31:0] data,
